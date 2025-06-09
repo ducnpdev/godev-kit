007021af54d2ab944d28700c1cda27212fa6ff0a
Your New Name <yournew.email@example.com>
2025-06-09 21:39:02 +0700

    feat: load env from config yaml

--- config/config.go
+++ config/config.go
@@ -2,82 +2,120 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
-	"github.com/caarlos0/env/v11"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 )
 
 type (
 	// Config -.
 	Config struct {
-		App     App
-		HTTP    HTTP
-		Log     Log
-		PG      PG
-		GRPC    GRPC
-		RMQ     RMQ
-		Metrics Metrics
-		Swagger Swagger
+		App     App     `mapstructure:"APP"`
+		HTTP    HTTP    `mapstructure:"HTTP"`
+		Log     Log     `mapstructure:"LOG"`
+		PG      PG      `mapstructure:"PG"`
+		GRPC    GRPC    `mapstructure:"GRPC"`
+		RMQ     RMQ     `mapstructure:"RMQ"`
+		Metrics Metrics `mapstructure:"METRICS"`
+		Swagger Swagger `mapstructure:"SWAGGER"`
 	}
 
 	// App -.
 	App struct {
-		Name    string `env:"APP_NAME" mapstructure:"APP_NAME"`
-		Version string `env:"APP_VERSION" mapstructure:"APP_VERSION"`
+		Name    string `mapstructure:"NAME"`
+		Version string `mapstructure:"VERSION"`
 	}
 
 	// HTTP -.
 	HTTP struct {
-		Port           string `env:"HTTP_PORT" mapstructure:"HTTP_PORT"`
-		UsePreforkMode bool   `env:"HTTP_USE_PREFORK_MODE" envDefault:"false" mapstructure:"HTTP_USE_PREFORK_MODE"`
+		Port           string `mapstructure:"PORT"`
+		UsePreforkMode bool   `mapstructure:"USE_PREFORK_MODE"`
 	}
 
 	// Log -.
 	Log struct {
-		Level string `env:"LOG_LEVEL" mapstructure:"LOG_LEVEL"`
+		Level string `mapstructure:"LEVEL"`
 	}
 
 	// PG -.
 	PG struct {
-		PoolMax int    `env:"PG_POOL_MAX" mapstructure:"PG_POOL_MAX"`
-		URL     string `env:"PG_URL" mapstructure:"PG_URL"`
+		PoolMax int    `mapstructure:"POOL_MAX"`
+		URL     string `mapstructure:"URL"`
 	}
 
 	// GRPC -.
 	GRPC struct {
-		Port string `env:"GRPC_PORT" mapstructure:"GRPC_PORT"`
+		Port string `mapstructure:"PORT"`
 	}
 
 	// RMQ -.
 	RMQ struct {
-		ServerExchange string `env:"RMQ_RPC_SERVER" mapstructure:"RMQ_RPC_SERVER"`
-		ClientExchange string `env:"RMQ_RPC_CLIENT" mapstructure:"RMQ_RPC_CLIENT"`
-		URL            string `env:"RMQ_URL" mapstructure:"RMQ_URL"`
+		ServerExchange string `mapstructure:"RPC_SERVER"`
+		ClientExchange string `mapstructure:"RPC_CLIENT"`
+		URL            string `mapstructure:"URL"`
 	}
 
 	// Metrics -.
 	Metrics struct {
-		Enabled bool `env:"METRICS_ENABLED" mapstructure:"METRICS_ENABLED" envDefault:"true"`
+		Enabled bool `mapstructure:"ENABLED"`
 	}
 
 	// Swagger -.
 	Swagger struct {
-		Enabled bool `env:"SWAGGER_ENABLED" mapstructure:"SWAGGER_ENABLED" envDefault:"false"`
-	}
-
-	// EnvValue configuration -.
-	EnvValue struct {
-		Type string `env:"TYPE" mapstructure:"TYPE" envDefault:"env"`
+		Enabled bool `mapstructure:"ENABLED"`
 	}
 )
 
+func (c *Config) binding(v *viper.Viper) error {
+	if err := v.Unmarshal(&c); err != nil {
+		log.Println("failed to unmarshal config: ", err)
+		return err
+	}
+	return nil
+}
+
 // NewConfig returns app config.
 func NewConfig() (*Config, error) {
+	return loadConfigYAML()
+}
 
-	cfg := &Config{}
+func loadConfigYAML() (*Config, error) {
+	conf := &Config{}
+	vn := viper.New()
 
-	if err := env.Parse(cfg); err != nil {
+	vn.AddConfigPath("config")
+	vn.SetConfigName("config")
+	vn.SetConfigType("yaml")
+	vn.AutomaticEnv()
+
+	vn.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	if err := vn.ReadInConfig(); err != nil {
 		return nil, fmt.Errorf("config error: %w", err)
 	}
 
-	return cfg, nil
+	for _, key := range vn.AllKeys() {
+		str := strings.ToUpper(strings.ReplaceAll(key, ".", "_"))
+
+		vn.BindEnv(key, str)
+	}
+
+	conf.binding(vn)
+	vn.WatchConfig()
+
+	vn.OnConfigChange(func(e fsnotify.Event) {
+		for _, key := range vn.AllKeys() {
+			str := strings.ToUpper(strings.ReplaceAll(key, ".", "_"))
+			log.Default().Println(key, str, vn.Get(key))
+			vn.BindEnv(key, str)
+		}
+
+		if err := conf.binding(vn); err != nil {
+			log.Println("binding error:", err)
+		}
+		log.Printf("config: %+v", conf)
+	})
+
+	return conf, nil
 }
