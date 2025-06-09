e87904a8cfcf51fc66ac9e4816becc28d16624ae
Your New Name <yournew.email@example.com>
2025-06-09 13:59:09 +0700

    feat: change and add mapstructure

--- config/config.go
+++ config/config.go
@@ -21,53 +21,60 @@ type (
 
 	// App -.
 	App struct {
-		Name    string `env:"APP_NAME,required"`
-		Version string `env:"APP_VERSION,required"`
+		Name    string `env:"APP_NAME" mapstructure:"APP_NAME"`
+		Version string `env:"APP_VERSION" mapstructure:"APP_VERSION"`
 	}
 
 	// HTTP -.
 	HTTP struct {
-		Port           string `env:"HTTP_PORT,required"`
-		UsePreforkMode bool   `env:"HTTP_USE_PREFORK_MODE" envDefault:"false"`
+		Port           string `env:"HTTP_PORT" mapstructure:"HTTP_PORT"`
+		UsePreforkMode bool   `env:"HTTP_USE_PREFORK_MODE" envDefault:"false" mapstructure:"HTTP_USE_PREFORK_MODE"`
 	}
 
 	// Log -.
 	Log struct {
-		Level string `env:"LOG_LEVEL,required"`
+		Level string `env:"LOG_LEVEL" mapstructure:"LOG_LEVEL"`
 	}
 
 	// PG -.
 	PG struct {
-		PoolMax int    `env:"PG_POOL_MAX,required"`
-		URL     string `env:"PG_URL,required"`
+		PoolMax int    `env:"PG_POOL_MAX" mapstructure:"PG_POOL_MAX"`
+		URL     string `env:"PG_URL" mapstructure:"PG_URL"`
 	}
 
 	// GRPC -.
 	GRPC struct {
-		Port string `env:"GRPC_PORT,required"`
+		Port string `env:"GRPC_PORT" mapstructure:"GRPC_PORT"`
 	}
 
 	// RMQ -.
 	RMQ struct {
-		ServerExchange string `env:"RMQ_RPC_SERVER,required"`
-		ClientExchange string `env:"RMQ_RPC_CLIENT,required"`
-		URL            string `env:"RMQ_URL,required"`
+		ServerExchange string `env:"RMQ_RPC_SERVER" mapstructure:"RMQ_RPC_SERVER"`
+		ClientExchange string `env:"RMQ_RPC_CLIENT" mapstructure:"RMQ_RPC_CLIENT"`
+		URL            string `env:"RMQ_URL" mapstructure:"RMQ_URL"`
 	}
 
 	// Metrics -.
 	Metrics struct {
-		Enabled bool `env:"METRICS_ENABLED" envDefault:"true"`
+		Enabled bool `env:"METRICS_ENABLED" mapstructure:"METRICS_ENABLED" envDefault:"true"`
 	}
 
 	// Swagger -.
 	Swagger struct {
-		Enabled bool `env:"SWAGGER_ENABLED" envDefault:"false"`
+		Enabled bool `env:"SWAGGER_ENABLED" mapstructure:"SWAGGER_ENABLED" envDefault:"false"`
+	}
+
+	// EnvValue configuration -.
+	EnvValue struct {
+		Type string `env:"TYPE" mapstructure:"TYPE" envDefault:"env"`
 	}
 )
 
 // NewConfig returns app config.
 func NewConfig() (*Config, error) {
+
 	cfg := &Config{}
+
 	if err := env.Parse(cfg); err != nil {
 		return nil, fmt.Errorf("config error: %w", err)
 	}
